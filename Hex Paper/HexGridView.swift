0e4b678e7d63ef382a219b935d3a4f10066e2b97
Michael Johnson <mj@notthatmj.com>
2017-05-16 18:03:47 -0500

    Draw hex in top left of document

--- Hex Paper/HexGridView.swift
+++ Hex Paper/HexGridView.swift
@@ -8,37 +8,51 @@
 
 import UIKit
 
+struct Hex {
+    var left: CGPoint?
+    var topLeft: CGPoint?
+    var topRight: CGPoint?
+    var right: CGPoint?
+    var bottomRight: CGPoint?
+    var bottomLeft: CGPoint?
+}
+
 @IBDesignable
 class HexGridView: UIView {
 
     // The heigh and width here we chosen because, when translated to PDF coordinates, I believe they'll
     // translate to an 8.5 x 11 piece of paper. But the choice is pretty arbitrary for the moment.
     override var intrinsicContentSize: CGSize { return CGSize(width: 612, height: 792)}
-
+    
     override func draw(_ rect: CGRect) {
-        // This is basically just some placeholder code for now. All it does is draw a single hex
-        // in the middle of the view.
-        let centerX = rect.midX
-        let centerY = rect.midY
-        let radius = CGFloat(10)
-        let angleIncrement = (2*Float.pi)/6
-        let initialAngle = Float(0)
-        let initialX = centerX + CGFloat(cos(initialTheta))*radius
-        let initialY = centerY + CGFloat(sin(initialTheta))*radius
-        let initialPoint = CGPoint(x: initialX, y: initialY)
-        
+        // Draw hex in the top left of the screen
+        let radius: Double = 20
+        let initialCenter = CGPoint(x: radius, y: radius*cos(Double.pi/6))
+        var initialHex = Hex()
+        initialHex.right = CGPoint(x: Double(initialCenter.x)+radius*cos((2*Double.pi/6)*0),
+                                   y: Double(initialCenter.y)+radius*sin((2*Double.pi / 6)*0))
+        initialHex.bottomRight = CGPoint(x: Double(initialCenter.x)+radius*cos((2*Double.pi/6)*1),
+                                         y: Double(initialCenter.y)+radius*sin((2*Double.pi / 6)*1))
+        initialHex.bottomLeft = CGPoint(x: Double(initialCenter.x)+radius*cos((2*Double.pi / 6)*2),
+                                        y: Double(initialCenter.y)+radius*sin((2*Double.pi / 6)*2))
+        initialHex.left = CGPoint(x: Double(initialCenter.x)+radius*cos((2*Double.pi/6)*3),
+                                  y: Double(initialCenter.y)+radius*sin((2*Double.pi / 6)*3))
+        initialHex.topLeft = CGPoint(x: Double(initialCenter.x)+radius*cos((2*Double.pi/6)*4),
+                                     y: Double(initialCenter.y)+radius*sin((2*Double.pi / 6)*4))
+        initialHex.topRight = CGPoint(x: Double(initialCenter.x)+radius*cos((2*Double.pi/6)*5),
+                                     y: Double(initialCenter.y)+radius*sin((2*Double.pi / 6)*5))
+
         let hexPath = UIBezierPath()
-        hexPath.move(to:initialPoint)
-        
-        for i in 1...5 {
-            let angle = initialTheta + angleIncrement * Float(i)
-            let nextX = centerX + CGFloat(cos(angle)) * radius
-            let nextY = centerY + CGFloat(sin(angle)) * radius
-            let nextPoint = CGPoint(x: nextX, y:nextY)
-            hexPath.addLine(to: nextPoint)
-        }
+        hexPath.move(to:initialHex.right!)
+        hexPath.addLine(to: initialHex.bottomRight!)
+        hexPath.addLine(to: initialHex.bottomLeft!)
+        hexPath.addLine(to: initialHex.left!)
+        hexPath.addLine(to: initialHex.topLeft!)
+        hexPath.addLine(to: initialHex.topRight!)
         hexPath.close()
+
         UIColor.gray.setStroke()
         hexPath.stroke()
+
     }
 }
