3e84607dce105d599a8dc6afb88cd4d24436d5cc
Michael Johnson <mj@notthatmj.com>
2017-05-16 16:47:35 -0500

    Draw a single hex in the middle of the document

--- Hex Paper/HexGridView.swift
+++ Hex Paper/HexGridView.swift
@@ -15,12 +15,30 @@ class HexGridView: UIView {
     // translate to an 8.5 x 11 piece of paper. But the choice is pretty arbitrary for the moment.
     override var intrinsicContentSize: CGSize { return CGSize(width: 612, height: 792)}
 
-    // We just draw a small line for now, to orient ourselves while panning and zooming.
     override func draw(_ rect: CGRect) {
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: rect.midX, y: rect.midY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY+20))
-        UIColor.green.setStroke()
-        path.stroke()
+        // This is basically just some placeholder code for now. All it does is draw a single hex
+        // in the middle of the view.
+        let centerX = rect.midX
+        let centerY = rect.midY
+        let radius = CGFloat(10)
+        let angleIncrement = (2*Float.pi)/6
+        let initialAngle = Float(0)
+        let initialX = centerX + CGFloat(cos(initialTheta))*radius
+        let initialY = centerY + CGFloat(sin(initialTheta))*radius
+        let initialPoint = CGPoint(x: initialX, y: initialY)
+        
+        let hexPath = UIBezierPath()
+        hexPath.move(to:initialPoint)
+        
+        for i in 1...5 {
+            let angle = initialTheta + angleIncrement * Float(i)
+            let nextX = centerX + CGFloat(cos(angle)) * radius
+            let nextY = centerY + CGFloat(sin(angle)) * radius
+            let nextPoint = CGPoint(x: nextX, y:nextY)
+            hexPath.addLine(to: nextPoint)
+        }
+        hexPath.close()
+        UIColor.gray.setStroke()
+        hexPath.stroke()
     }
 }
