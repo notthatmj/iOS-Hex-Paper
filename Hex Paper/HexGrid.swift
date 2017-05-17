05e95db934ee4baac54c5d75019589d6ad6a2abf
Michael Johnson <mj@notthatmj.com>
2017-05-16 19:27:52 -0500

    Checkpoint

---
+++ Hex Paper/HexGrid.swift
@@ -0,0 +1,13 @@
+//
+//  HexGrid.swift
+//  Hex Paper
+//
+//  Created by Michael Johnson on 5/16/17.
+//  Copyright © 2017 Michael Johnson. All rights reserved.
+//
+
+import Foundation
+
+class HexGrid {
+    
+}
---
+++ Hex PaperTests/HexGridTests.swift
@@ -0,0 +1,19 @@
+//
+//  HexGridTests.swift
+//  Hex Paper
+//
+//  Created by Michael Johnson on 5/16/17.
+//  Copyright © 2017 Michael Johnson. All rights reserved.
+//
+
+import XCTest
+@testable import Hex_Paper
+
+class HexGridTests: XCTestCase {
+    
+    func testHexGrid() {
+        let SUT = HexGrid(width: 100, height: 200)
+        
+        
+    }
+}
